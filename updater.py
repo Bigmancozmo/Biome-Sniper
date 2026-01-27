3d3d64c338e75ee1b2eb01264ee5ab19536f92a6
Bigmancozmo <bigmancozmo@proton.me>
2026-01-26 19:44:27 -0500

    Version bump

--- updater.py
+++ updater.py
@@ -2,7 +2,7 @@ import requests
 import os, zipfile
 
 DISABLE_SKIP = False
-CURRENT = "v1.0.0"
+CURRENT = "v1.0.1"
 
 # Set this environment variable to disable the auto updater in that directory
 value = os.environ.get("BIOME_SNIPER_DEV_FOLDER").upper()
