262591b318b3a732c7cdaa017712a172b574ca1b
Bigmancozmo <bigmancozmo@proton.me>
2026-01-29 20:42:07 -0500

    Channel Scanner - usage TODO

---
+++ server-scan/blacklist.py
@@ -0,0 +1,28 @@
+# Channels that are known to not be useful
+# Feel free to contribute!
+
+BLACKLIST_CATEGORIES = [
+	# Sol's RNG Discord
+	"1187753320710938665", # Entry
+	"1360920055315038229", # Important
+	"1391149466660114522", # Engagement
+	"1319743102168207451", # Community
+	"1186570213077041234", # Main
+	"1360944887037558903", # Korean
+	"1265010160946905118", # Stats Found
+	"1186577974393307186", # Feedback,
+	"1207791867601752105", # Aura Creation
+	"1414714197153153024", # AC Essentials
+	"1186570213077041235", # VC channels
+	"1434136703022534856", # Tickets 1
+	"1463125634384527567", # Tickets 2
+	"1211506038289731684", # Archive
+	"1213762738158047282", # Unverified
+]
+
+# You don't need to put channels blacklisted
+# by the category list above
+BLACKLIST_CHANNELS = [
+	# Sol's RNG Discord
+	"1282554696032194593", #others
+]
---
+++ server-scan/scanner.py
@@ -0,0 +1,60 @@
+from concurrent.futures import ThreadPoolExecutor
+import requests
+import dataMgr as d
+from logging import warning as warn
+import sys, util
+from blacklist import BLACKLIST_CHANNELS, BLACKLIST_CATEGORIES
+
+sys.stdout.reconfigure(encoding="utf-8")
+
+def getHeaders():
+	token = d.get_key("DiscordToken", "")
+	return {"Authorization": token}
+
+TOTAL = 999
+PROGRESS = 0
+
+def processChannel(channel):
+	global TOTAL, PROGRESS
+	PROGRESS += 1
+	print(f"Channel {PROGRESS}/{TOTAL} ({channel["name"]})")
+
+	if channel["type"] != 0:
+		return
+	if channel["parent_id"] != None:
+		if channel["parent_id"] in BLACKLIST_CATEGORIES:
+			warn(f"Category {channel["parent_id"]} is blacklisted!")
+			return
+	if channel["id"] in BLACKLIST_CHANNELS or channel["id"] in BLACKLIST_CATEGORIES:
+		warn(f"Channel {channel["id"]} is blacklisted!")
+		return
+	
+	channelId = channel["id"]
+	channelName = channel["name"]
+	endpoint = f"https://discord.com/api/v9/channels/{channelId}/messages?limit=5"
+	r = requests.get(endpoint, headers=getHeaders())
+	linkMessages = 0
+	if r.status_code != 200:
+		warn(f"No permission: {channelId} ({r.status_code}) Category={channel["parent_id"]} Name={channel["name"]}")
+		return
+	for msg in r.json():
+		if util.hasLink(msg):
+			linkMessages += 1
+		if linkMessages >= 2:
+			break
+	if linkMessages >= 2:
+		print("HIT:", channelId, channelName)
+
+def scanServer(serverId):
+	global TOTAL, PROGRESS
+	TOTAL = 1
+	PROGRESS = 0
+	endpoint = f"https://discord.com/api/v10/guilds/{serverId}/channels"
+	r = requests.get(endpoint, headers=getHeaders())
+	if r.status_code != 200:
+		warn(f"Failed to find channels for server {serverId}")
+		return
+	channels = r.json()
+	TOTAL = len(channels)
+	with ThreadPoolExecutor(max_workers=10) as pool:
+		pool.map(processChannel, channels)
