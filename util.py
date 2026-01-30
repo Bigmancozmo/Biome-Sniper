139b96c2b83b71662f532cc8a9e444b7759d2110
Bigmancozmo <bigmancozmo@proton.me>
2026-01-29 20:41:43 -0500

    New system

--- util.py
+++ util.py
@@ -2,24 +2,24 @@ import discord
 
 def extractText(message, filter=False):
 	allText = ""
-	if message.content:
-		allText = allText + message.content
-		if message.embeds:
-			for embed in message.embeds:
-				allText = allText + str(embed.title)
-				allText = allText + str(embed.description)
-		if message.components:
+	if message["content"]:
+		allText = allText + message["content"]
+		if message["embeds"]:
+			for embed in message["embeds"]:
+				allText = allText + str(embed["title"])
+				allText = allText + str(embed["description"])
+		if message["components"]:
 			# For MultiScope 2.0.0
 			# embeds have a "Join Server" button rather than a link.
 			# I dont know if MS2 uses ActionRow or Button so I just check both
-			for component in message.components:
+			for component in message["components"]:
 				if component.type == discord.ComponentType.button:
-					allText = allText + action.url
+					allText = allText + action["url"]
 				else:
 					if component.type == discord.ComponentType.action_row:
-						for action in component.children:
+						for action in component["children"]:
 							if action.type == discord.ComponentType.button:
-								allText = allText + action.url
+								allText = allText + action["url"]
 	
 	if filter:
 		allText = allText.replace(" ", "")
@@ -33,6 +33,6 @@ def extractText(message, filter=False):
 
 def hasLink(message):
 	text = extractText(message)
-	if "roblox.com/games/15532962292" in text:
+	if ("roblox.com/games/15532962292" in text) or ("roblox.com/share" in text):
 		return True
 	return False
